a9c8fdf022744c5b8855f3594475790499483bec
Towhid Islam <m.towhid.islam@gmail.com>
2022-11-16 07:25:01 +0600

    refactoring model properties

--- src/main/java/com/infoworks/lab/domain/models/Transaction.java
+++ src/main/java/com/infoworks/lab/domain/models/Transaction.java
@@ -3,13 +3,11 @@ package com.infoworks.lab.domain.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
-
 public class Transaction extends CreateAccount {
     @JsonIgnore private String accountType;
 
-    @NotNull(message = "type must not be null. Any string less-then 20 char. e.g. transaction, purchase, transfer etc")
-    @Length(max = 20, min = 1, message = "type has to be 1<=length<=20")
+    //@NotNull(message = "type must not be null. Any string less-then 20 char. e.g. transaction, purchase, transfer etc")
+    //@Length(max = 20, min = 1, message = "type has to be 1<=length<=20")
     private String type;
 
     /**
