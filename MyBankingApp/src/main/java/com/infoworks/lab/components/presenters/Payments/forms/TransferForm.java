2d57ee4675ee06bdb6a17b3a7025e9db5f34ef56
Towhid Islam <m.towhid.islam@gmail.com>
2025-05-30 09:58:17 +0200

    ui-app:feat: adding transaction-view and forms.

---
+++ MyBankingApp/src/main/java/com/infoworks/lab/components/presenters/Payments/forms/TransferForm.java
@@ -0,0 +1,245 @@
+package com.infoworks.lab.components.presenters.Payments.forms;
+
+import com.infoworks.lab.components.component.FormActionBar;
+import com.infoworks.lab.components.component.IndeterminateDialog;
+import com.infoworks.lab.config.ApplicationProperties;
+import com.infoworks.lab.config.ValidationConfig;
+import com.infoworks.lab.domain.beans.queues.EventQueue;
+import com.infoworks.lab.domain.models.payments.TransactionAmount;
+import com.infoworks.lab.domain.models.payments.VAccountResponseParser;
+import com.infoworks.lab.domain.repository.AuthRepository;
+import com.infoworks.lab.domain.repository.VAccountRepository;
+import com.infoworks.lab.rest.models.Response;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.textfield.BigDecimalField;
+
+import javax.validation.Validator;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class TransferForm extends FormLayout {
+
+    private final String fromPrefix;
+    private final String fromAccountName;
+    private final String fromAccountTitle;
+    private final String toPrefix;
+    private final String toAccountName;
+    private final String toAccountTitle;
+    //
+    private Label fromAccountTitleLb = new Label("Sender: ");
+    private Label fromAccountBalanceLb = new Label("Balance: ");
+    private BigDecimal fromCurrentBalance = new BigDecimal("0.00");
+    //
+    private Label toAccountTitleLb = new Label("Receiver: ");
+    private Label toAccountBalanceLb = new Label("Balance: ");
+    private BigDecimal toCurrentBalance = new BigDecimal("0.00");
+    //
+    private BigDecimalField transferAmount = new BigDecimalField("Transfer:");
+    //
+    private FormActionBar actionBar;
+    private VAccountRepository repository;
+    private Dialog dialog;
+
+    public TransferForm(String fromPrefix
+            , String fromAccountName
+            , String toPrefix
+            , String toAccountName
+            , Dialog dialog) {
+        //
+        this.fromPrefix = fromPrefix;
+        this.fromAccountName = fromAccountName;
+        this.fromAccountTitle = String.format("%s@%s", this.fromPrefix, this.fromAccountName);
+        //
+        this.toPrefix = toPrefix;
+        this.toAccountName = toAccountName;
+        this.toAccountTitle = String.format("%s@%s", this.toPrefix, this.toAccountName);
+        //
+        this.actionBar = new FormActionBar(dialog);
+        this.repository = new VAccountRepository(AuthRepository.parseToken(UI.getCurrent()));
+        this.dialog = dialog;
+    }
+
+    public TransferForm(String fromPrefix
+            , String fromAccountName
+            , String toPrefix
+            , String toAccountName) {
+        this(fromPrefix, fromAccountName, toPrefix, toAccountName, null);
+    }
+
+    public String getFromAccountTitle() {
+        return fromAccountTitle;
+    }
+
+    public String getFromPrefix() {
+        return fromPrefix;
+    }
+
+    public String getFromAccountName() {
+        return fromAccountName;
+    }
+
+    public String getToAccountTitle() {
+        return toAccountTitle;
+    }
+
+    public String getToPrefix() {
+        return toPrefix;
+    }
+
+    public String getToAccountName() {
+        return toAccountName;
+    }
+
+    public boolean isModalWindow() {
+        return this.dialog != null;
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        //
+        this.fromAccountTitleLb.setText(String.format("Sender: %s", getFromAccountTitle()));
+        this.toAccountTitleLb.setText(String.format("Receiver: %s", getToAccountTitle()));
+        //
+        this.transferAmount.setRequiredIndicatorVisible(true);
+        this.transferAmount.setValue(new BigDecimal("0.00"));
+        //
+        this.actionBar.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.STRETCH);
+        this.actionBar.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+        this.actionBar.setSaveButtonDisableOnClick(true);
+        this.actionBar.addOnSaveAction(onSaveAction());
+        //
+        add(fromAccountTitleLb, fromAccountBalanceLb
+                , toAccountTitleLb, toAccountBalanceLb
+                , transferAmount
+                , actionBar);
+        //UI config:
+        setResponsiveSteps(
+                // Use one column by default
+                new ResponsiveStep("0", 1),
+                // Use two columns, if layout's width exceeds 500px
+                new ResponsiveStep("300px", 2)
+        );
+        // Stretch the username & actionBar field over 2 columns
+        setColspan(transferAmount, 2);
+        setColspan(actionBar, 2);
+        //Fetch User Account-Info: Update accountBalance
+        fetchBalance(UI.getCurrent());
+    }
+
+    private void fetchBalance(UI ui) {
+        Dialog dialog = new IndeterminateDialog("Fetching balance...");
+        dialog.addAttachListener(event -> {
+            EventQueue.dispatch(500, TimeUnit.MILLISECONDS
+                    , () -> ui.access(() -> {
+                        //Fetch and update:
+                        try {
+                            Response response = repository.accountBalance(getFromPrefix(), getFromAccountName());
+                            fromCurrentBalance = VAccountResponseParser.getBalance(response);
+                            fromAccountBalanceLb.setText("Balance: " + fromCurrentBalance.toString());
+                            //
+                            response = repository.accountBalance(getToPrefix(), getToAccountName());
+                            toCurrentBalance = VAccountResponseParser.getBalance(response);
+                            toAccountBalanceLb.setText("Balance: " + toCurrentBalance.toString());
+                        } catch (RuntimeException e) {
+                            Notification notification = Notification.show(e.getLocalizedMessage());
+                            notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                        }
+                        dialog.close();
+                    }));
+        });
+        dialog.open();
+    }
+
+    private ComponentEventListener<ClickEvent<Button>> onSaveAction() {
+        return (event) -> {
+            //On Save Action:
+            UI ui = event.getSource().getUI().orElse(null);
+            //
+            BigDecimal depositAmount = Optional.ofNullable(this.transferAmount.getValue())
+                    .orElse(new BigDecimal("0.00"));
+            TransactionAmount transactionAmount = new TransactionAmount(depositAmount.toString());
+            //
+            Validator validator = ValidationConfig.getValidator();
+            String messages = ValidationConfig.validateWithMessage(validator, transactionAmount);
+            if (messages != null) {
+                Notification notification = Notification.show(messages);
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                this.actionBar.setSaveButtonEnable(true);
+            } else {
+                boolean isFromAccountBalanceEqualOrLess = fromCurrentBalance.compareTo(new BigDecimal("0.00")) <= 0;
+                boolean isTransactionEqualOrLess = transactionAmount.getMoney().equals("0.00");
+                if (isFromAccountBalanceEqualOrLess || isTransactionEqualOrLess) {
+                    String msg = isTransactionEqualOrLess
+                            ? "Transfer not possible, amount is 0.00!"
+                            : "Transfer not possible, Sender Balance is 0.00!";
+                    Notification notification = Notification.show(msg);
+                    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    this.actionBar.setSaveButtonEnable(true);
+                } else {
+                    saveTransferAmount(ui, transactionAmount);
+                }
+            }
+        };
+    }
+
+    private void saveTransferAmount(UI ui, TransactionAmount transactionAmount) {
+        Dialog dialog = new IndeterminateDialog("Transfer balance...");
+        dialog.addAttachListener(event -> {
+            EventQueue.dispatch(500, TimeUnit.MILLISECONDS
+                    , () -> ui.access(() -> {
+                        //Save Deposit:
+                        try {
+                            Response response = repository.accountExist(getFromPrefix(), getFromAccountName());
+                            boolean isFromAccountExist = VAccountResponseParser.isExist(response);
+                            //
+                            response = this.repository.accountExist(getToPrefix(), getToAccountName());
+                            boolean isToAccountExist = VAccountResponseParser.isExist(response);
+                            //
+                            if (isFromAccountExist && isToAccountExist) {
+                                String amount = transactionAmount.getMoney();
+                                response = repository.makeTransaction(
+                                        getFromPrefix()
+                                        , getFromAccountName()
+                                        , ApplicationProperties.CURRENCY
+                                        , amount
+                                        , getToAccountTitle());
+                                //
+                                if (response.getStatus() == 200) {
+                                    fromCurrentBalance = fromCurrentBalance.subtract(new BigDecimal(amount));
+                                    fromAccountBalanceLb.setText("Balance: " + fromCurrentBalance.toString());
+                                    //
+                                    toCurrentBalance = toCurrentBalance.add(new BigDecimal(amount));
+                                    toAccountBalanceLb.setText("Balance: " + toCurrentBalance.toString());
+                                } else {
+                                    Notification notification = Notification.show(response.getError());
+                                    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                                }
+                            } else {
+                                //
+                                Notification notification = Notification.show("Transfer can not be possible, accounts are unavailable!");
+                                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                            }
+                        } catch (Exception e) {
+                            Notification notification = Notification.show(e.getMessage());
+                            notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                        }
+                        actionBar.setSaveButtonEnable(true);
+                        dialog.close();
+                    }));
+        });
+        dialog.open();
+    }
+
+}
---
+++ MyBankingApp/src/main/java/com/infoworks/lab/components/presenters/Payments/views/TransactionsView.java
@@ -0,0 +1,254 @@
+package com.infoworks.lab.components.presenters.Payments.views;
+
+import com.infoworks.lab.components.component.FormActionBar;
+import com.infoworks.lab.components.component.IndeterminateDialog;
+import com.infoworks.lab.domain.beans.queues.EventQueue;
+import com.infoworks.lab.domain.models.payments.Transaction;
+import com.infoworks.lab.domain.models.payments.TransactionType;
+import com.infoworks.lab.domain.repository.VAccountRepository;
+import com.infoworks.lab.rest.models.SearchQuery;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class TransactionsView extends Composite<Div> {
+    //Components:
+    private HorizontalLayout navBar;
+    private Grid<Transaction> mainView;
+    private ComboBox<TransactionType> transactionTypeBox = new ComboBox<>("Type:", TransactionType.values());
+    private final Integer[] pageSizeItems = new Integer[]{5, 10, 15, 20, 50};
+    private ComboBox<Integer> pageSizeBox = new ComboBox<>("Page Size:", pageSizeItems);
+    private final DatePicker startDatePicker = new DatePicker("From Date:");
+    private final DatePicker endDatePicker = new DatePicker("To Date");
+    //Attributes:
+    private String accountName;
+    private String accountPrefix;
+    private VAccountRepository repository;
+    private boolean isModalView;
+    private FormActionBar actionBar;
+
+    public TransactionsView(String accountPrefix, String accountName, Dialog dialog) {
+        this.accountPrefix = accountPrefix;
+        this.accountName = accountName;
+        this.repository = new VAccountRepository();
+        this.actionBar = new FormActionBar(dialog);
+        this.isModalView = dialog != null;
+    }
+
+    public TransactionsView(String accountPrefix, String accountName) {
+        this(accountPrefix, accountName, null);
+    }
+
+    public TransactionsView() {
+        this(null, null);
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        super.onDetach(detachEvent);
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        if (getContent().getChildren().count() > 0){
+            getContent().removeAll();
+        }
+        super.onAttach(attachEvent);
+        //
+        this.navBar = initNavBar();
+        this.mainView = initGridView(20);
+        //
+        VerticalLayout parent = new VerticalLayout();
+        parent.setSpacing(true);
+        parent.setSizeFull();
+        if (this.isModalView) {
+            this.actionBar.setPadding(false);
+            this.actionBar.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+            this.actionBar.setWidthFull();
+            parent.add(this.navBar, this.mainView, this.actionBar);
+        } else {
+            parent.add(this.navBar, this.mainView);
+        }
+        getContent().add(parent);
+        //Send default search:
+        update(UI.getCurrent(), this.accountPrefix, this.accountName);
+    }
+
+    public void update(UI ui, String prefix, String accountName) {
+        this.accountPrefix = prefix;
+        this.accountName = accountName;
+        //Send default search:
+        if (this.accountPrefix != null && !this.accountPrefix.isEmpty()) {
+            //Pick field values:
+            int pageSize = this.pageSizeBox.getValue();
+            TransactionType type = this.transactionTypeBox.getValue();
+            LocalDate from = this.startDatePicker.getValue();
+            LocalDate to = this.endDatePicker.getValue();
+            //Start: Dispatch-Async
+            if (pageSize <= this.pageSizeItems[0]) {
+                EventQueue.dispatch(500
+                        , TimeUnit.MILLISECONDS
+                        , () -> ui.access(() -> {
+                            List<Transaction> transactions = triggerDataFetch(ui
+                                    , this.accountPrefix, this.accountName
+                                    , pageSize, type
+                                    , from, to);
+                            //Update UI:
+                            this.mainView.setItems(transactions);
+                        }));
+            } else {
+                //Open delay & progress with:
+                Dialog dialog = new IndeterminateDialog("Please wait...");
+                dialog.addAttachListener(event -> {
+                    //Start rest call with some delay: 1.2 sec
+                    EventQueue.dispatch(1200, TimeUnit.MILLISECONDS
+                            , () -> ui.access(() -> {
+                                //Pass values:
+                                List<Transaction> transactions = triggerDataFetch(ui
+                                        , this.accountPrefix, this.accountName
+                                        , pageSize, type
+                                        , from, to);
+                                //Update UI:
+                                this.mainView.setItems(transactions);
+                                dialog.close();
+                            }));
+                });
+                dialog.open();
+            }
+            //End: Dispatch-Async
+        } else {
+            ui.access(() -> this.mainView.setItems(new ArrayList<>()));
+        }
+    }
+
+    private Grid<Transaction> initGridView(int defaultPageSize) {
+        Grid<Transaction> grid = new Grid<>(Transaction.class, false);
+        grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        grid.setPageSize(defaultPageSize);
+        grid.setAllRowsVisible(true);
+        grid.setWidthFull();
+        //Columns
+        grid.addColumn(Transaction::getAccountPrefix)
+                .setHeader("Prefix")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getAccountName)
+                .setHeader("AccountName")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getCurrency)
+                .setHeader("Currency")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getAmount)
+                .setHeader("Amount")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getBalance)
+                .setHeader("Balance")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getTransaction_type)
+                .setHeader("Type")
+                .setAutoWidth(true);
+
+        grid.addColumn(Transaction::getTransaction_date)
+                .setHeader("Date")
+                .setAutoWidth(true);
+        //
+        return grid;
+    }
+
+    private HorizontalLayout initNavBar() {
+        //Setup UIs:
+        this.pageSizeBox.setValue(pageSizeItems[0]); //Default value
+        this.transactionTypeBox.setValue(TransactionType.Any); //Default value
+        //From Datetime field:
+        this.startDatePicker.setMin(LocalDate.now().minusMonths(3));
+        this.startDatePicker.setMax(LocalDate.now().plusMonths(6));
+        this.startDatePicker.setValue(LocalDate.now().minusDays(7));
+        //To Datetime field:
+        this.endDatePicker.setMin(LocalDate.now().minusMonths(3));
+        this.endDatePicker.setMax(LocalDate.now().plusMonths(6));
+        this.endDatePicker.setValue(LocalDate.now().plusDays(1));
+        //Fetch Action Button:
+        Button doFetch = new Button("Search", VaadinIcon.SEARCH.create());
+        doFetch.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        doFetch.addClickListener(e -> {
+            //Send default search:
+            UI ui = e.getSource().getUI().orElse(null);
+            update(ui, this.accountPrefix, this.accountName);
+        });
+        //
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.BASELINE);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.START);
+        layout.add(pageSizeBox, transactionTypeBox, startDatePicker, endDatePicker, doFetch);
+        return layout;
+    }
+
+    private List<Transaction> triggerDataFetch(UI ui, String prefix
+            , String username
+            , int pageSize
+            , TransactionType type
+            , LocalDate from
+            , LocalDate to) {
+        //Prepare Search-Query:
+        SearchQuery query = new SearchQuery();
+        query.setSize(pageSize);
+        query.setPage(0);
+        //Adding Transaction-type:
+        if (type != TransactionType.Any)
+            query.add("type").isEqualTo(type.value());
+        //Assigning From or To datetime:
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String fromDate = from.format(dateFormat);
+        query.add("from").isGreaterThenOrEqual(fromDate);
+        //
+        String toDate = to.format(dateFormat);
+        query.add("to").isGreaterThenOrEqual(toDate);
+        //query.add("till").isGreaterThenOrEqual(<till-date>);
+        //Send search-request:
+        try {
+            List<Map> response = repository.searchTransactions(prefix, username, query);
+            //Sort by transaction_date & balance descending order:
+            Comparator sortBy = Comparator.comparing(Transaction::getTransactionLocalDate)
+                    .thenComparing(Transaction::getBalance).reversed();
+            //Sort by balance descending order:
+            //sortBy = Comparator.comparing(Transaction::getBalance).reversed();
+            //
+            List<Transaction> transactions = repository.convert(response, sortBy);
+            return transactions;
+            //
+        } catch (Exception e) {
+            ui.access(() -> {
+                Notification notification = Notification.show(e.getLocalizedMessage());
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+            });
+        }
+        return new ArrayList<>();
+    }
+}
